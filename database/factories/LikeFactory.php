5ec92c98e8065ee06c500d75d942684e024d86c0
SRBarua <baruasudipta@gmail.com>
2019-09-14 12:15:09 -0600

    like

--- database/factories/LikeFactory.php
+++ database/factories/LikeFactory.php
@@ -5,7 +5,7 @@
 use App\Like;
 use Faker\Generator as Faker;
 
-$factory->define(Like::class, function (Faker $faker) {
+$factory->define(like::class, function (Faker $faker) {
     return [
         'tweet_id'  =>  $faker->unique()->numberBetween(1, 25 * $GLOBALS['userCount'])
     ];
